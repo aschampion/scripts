d484f49859b972d0bf2ef494c5c8b27e295e2033
Andrew Champion <andrew.champion@gmail.com>
2020-10-25 10:43:33 +0000

    Use 2 slot for DAT conversion to avoid OOM for large sections

--- dataproc/cluster/fibsem-dat/convert.sh
+++ dataproc/cluster/fibsem-dat/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Designed for 24 slots
+# Designed for 2 slots
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-~/janeliahome/alignment/Fiji.app/ImageJ-linux64 -Xms2g -Xmx2g -- --ij2 --headless --console --run $DIR/merlin-dat-to-tiff.groovy 'datFile="'$1'"'
+~/janeliahome/alignment/Fiji.app/ImageJ-linux64 -Xms5g -Xmx5g -- --ij2 --headless --console --run $DIR/merlin-dat-to-tiff.groovy 'datFile="'$1'"'
 
