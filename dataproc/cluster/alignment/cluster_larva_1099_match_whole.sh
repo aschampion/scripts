ecab39ebec758f202c1091d28ef2212d4bb489b0
Andrew Champion <andrew.champion@gmail.com>
2020-11-03 12:36:27 +0000

    Alignment: fix path in 1099 script

--- dataproc/cluster/alignment/cluster_larva_1099_match_whole.sh
+++ dataproc/cluster/alignment/cluster_larva_1099_match_whole.sh
@@ -2,5 +2,5 @@
 
 # Designed for 24 slots on the LMB cluster
 
-~/alignment/Fiji.app/ImageJ-linux64 -XX:ActiveProcessorCount=12 -Xms62g -Xmx62g -- --ij2 --headless --run ~/alignment/acardona-scripts/python/imagej/FIBSEM/serial_section_registration_1085.py 'SOURCE_DIR="/beegfs3/achampion/Larva1099/",TARGET_DIR="/lmb/home/achampion/alignment/output/1099/v1/",ENSURE_MATCHES=true,ALIGN=false,EXIT=true,CHUNK_COUNT=22,CHUNK_LIMIT=1,CHUNK_START='$1
+~/alignment/Fiji.app/ImageJ-linux64 -XX:ActiveProcessorCount=12 -Xms62g -Xmx62g -- --ij2 --headless --run ~/alignment/acardona-scripts/python/imagej/FIBSEM/serial_section_registration_1085.py 'SOURCE_DIR="/beegfs3/achampion/Larva1099/",TARGET_DIR="/beegfs3/achampion/alignment/output/1099/v1/",ENSURE_MATCHES=true,ALIGN=false,EXIT=true,CHUNK_COUNT=22,CHUNK_LIMIT=1,CHUNK_START='$1
 
